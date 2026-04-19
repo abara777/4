7ed589f7d9bb1cd228149ea7ad596c78a864abab
abara777 <abarakatabara777@gmail.com>
2026-04-19 17:09:11 +0300

    Update daysteps 1.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -68,5 +68,4 @@ func DayActionInfo(data string, weight, height float64) string {
 	}
 
 	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, burnt)
-
 }
